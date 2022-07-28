b00ddb3ad4e9815071cf91e4435bd301888d68c2
shahram <shahramkhaleh2001@gmail.com>
2022-07-28 11:06:50 -0700

    add SErvicesplus Section

--- js/main.js
+++ js/main.js
@@ -3,3 +3,22 @@ const navBar = document.querySelector(".nav");
 toggler.addEventListener("click", () => {
   navBar.classList.toggle("nav__expanded");
 });
+
+// plus services
+const tabs = document.querySelectorAll(".tab");
+const tabContects = document.querySelectorAll(".tab-content");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const targetTabContent = document.querySelector(
+      `#${tab.dataset.tabTarget}`
+    );
+
+    tabs.forEach((tab) => tab.classList.remove("active"));
+    console.log(tab);
+    tabContects.forEach((tabContect) => tabContect.classList.remove("active"));
+
+    tab.classList.add("active");
+    targetTabContent.classList.add("active");
+  });
+});
